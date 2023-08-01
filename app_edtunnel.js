3adf1d8b9a4bb591838b9a9111cebed1d9dc78eb
edwardzxx <edwardzlau@gmail.com>
2023-08-01 14:51:09 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'f507a3b7-abab-47a2-8fcf-e395b3b9efcc';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
